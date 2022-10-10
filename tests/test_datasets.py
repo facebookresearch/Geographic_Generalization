ffcb2ae00c71858ec7cfa708131f99a6f0fa1ba9
marksibrahim <mark.ibhm@gmail.com>
2022-10-10 17:00:35 -0400

    passing unit tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -4,7 +4,7 @@ from datasets.imagenet import ImageNetDataModule
 class TestImageNet:
     def test_imagenet(self):
         dm = ImageNetDataModule(batch_size=8)
-        val_batch = dm.val_dataloader()[0]
-        x, y = val_batch
+        val_batch = dm.val_dataloader()
+        x, y = next(iter(val_batch))
         assert x.shape == (8, 3, 224, 224)
-        assert y.shape == (8, 1)
+        assert y.shape == (8,)
