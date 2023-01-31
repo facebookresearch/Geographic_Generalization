2543624530f03c2620ccc8ae76623f04eee03d0c
Megan Richards <meganrichards@devfair0175.h2.fair>
2023-01-31 08:10:33 -0800

    datamodules have basic parent class with dataset name property

--- datasets/dummy.py
+++ datasets/dummy.py
@@ -1,6 +1,10 @@
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 import torch
+from datasets.image_datamodule import ImageDataModule
+import pytorch_lightning as pl
+from torchvision import transforms as transform_lib
+from torch.utils.data import DataLoader
 
 
 class ImageDataset(Dataset):
@@ -19,12 +23,6 @@ class ImageDataset(Dataset):
         self.len = num_samples
         self.x = torch.rand(num_samples, 3, 224, 224)
         self.labels = torch.randint(num_classes, (num_samples,))
-        self.fov = {
-            "pose_x": 0,
-            "pose_y": 0,
-            "pose_z": 0,
-            "image_path": "great_path.png",
-        }
 
     def __getitem__(self, index):
         return self.x[index], self.labels[index]
@@ -33,31 +31,37 @@ class ImageDataset(Dataset):
         return self.len
 
 
-class DummyDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size: int = 8, num_classes: int = 55):
+class DummyDataModule(ImageDataModule):
+    def __init__(
+        self,
+        batch_size: int = 8,
+        num_classes: int = 55,
+        image_size: int = 224,
+    ):
         """Randomly generated ('dummy') image dataset designed to be used with ImageNet models.
 
         Args:
             batch_size (int, optional): _description_. Defaults to 8.
             num_classes (int, optional): _description_. Defaults to 55.
         """
-        super().__init__()
-        self.batch_size = batch_size
+        super().__init__(
+            data_dir="",
+            batch_size=batch_size,
+            image_size=image_size,
+        )
         self.num_classes = num_classes
-        self.ds = ImageDataset(num_classes=self.num_classes)
 
-        self.train_loader_names = ["train"]
-        self.val_loader_names = ["val"]
-        self.test_loader_names = ["test_1"]
-
-    def train_dataloader(self):
-        return DataLoader(self.ds, batch_size=self.batch_size, num_workers=1)
-
-    def val_dataloader(self):
-        return DataLoader(self.ds, batch_size=self.batch_size, num_workers=1)
-
-    def test_dataloader(self):
-        return DataLoader(self.ds, batch_size=self.batch_size, num_workers=1)
+    def _create_dataloader(self, stage: str, augmentations: transform_lib.Compose):
+        shuffle = True if stage == "train" else False
+        dataset = ImageDataset(num_classes=self.num_classes)
+        data_loader = DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            pin_memory=True,
+            num_workers=self.num_workers,
+            shuffle=shuffle,
+        )
+        return data_loader
 
 
 if __name__ == "__main__":
