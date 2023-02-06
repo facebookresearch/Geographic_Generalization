fdd095a2bf285e6e25d12e305d3947f4b379492e
Megan Richards <meganrichards@devfair0175.h2.fair>
2023-02-06 13:45:05 -0800

    tests for v2 and R

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,14 +1,40 @@
 from datasets.dummy import DummyDataModule
 from datasets.imagenet import ImageNetDataModule
+from datasets.imagenet_rendition import ImageNetRenditionDataModule
 from pathlib import Path
 
 
 class TestImageNet:
     def test_imagenet(self):
         dm = ImageNetDataModule(batch_size=8)
-        assert Path(dm.data_dir, "val").exists()
-        val_batch = dm.val_dataloader()
-        x, y = next(iter(val_batch))
+        assert Path(dm.data_dir, "test").exists()
+        test_batch = dm.test_dataloader()
+        x, y = next(iter(test_batch))
+        assert x.shape == (8, 3, 224, 224)
+        assert y.shape == (8,)
+
+
+class TestImageNetV2:
+    def test_imagenetv2(self):
+        dm = ImageNetDataModule(
+            data_dir="/checkpoint/meganrichards/datasets/imagenetv2-matched-frequency/",
+            batch_size=8,
+        )
+        assert Path(dm.data_dir, "test").exists()
+        test_batch = dm.test_dataloader()
+        x, y = next(iter(test_batch))
+        assert x.shape == (8, 3, 224, 224)
+        assert y.shape == (8,)
+
+
+class TestImageNetR:
+    def test_imagenetv2(self):
+        dm = ImageNetRenditionDataModule(
+            batch_size=8,
+        )
+        assert Path(dm.data_dir, "test").exists()
+        test_batch = dm.test_dataloader()
+        x, y = next(iter(test_batch))
         assert x.shape == (8, 3, 224, 224)
         assert y.shape == (8,)
 
