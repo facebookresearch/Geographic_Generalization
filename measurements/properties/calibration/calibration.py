ab6246e4a4b4a8de1b2c84178d9c0b2dd7038e62
Polina Kirichenko <polkirichenko@devfair0811.h2.fair>
2023-02-21 01:00:52 -0800

    fixed make_new_test_step

--- measurements/properties/calibration/calibration.py
+++ measurements/properties/calibration/calibration.py
@@ -1,11 +1,9 @@
 from measurements.measurement_utils import Measurement
 from omegaconf import DictConfig
-import torch
-from measurements.properties.equivariance import transformations
-from torch import Tensor
 from typing import List, Dict
 from models.classifier_model import ClassifierModule
 import torch.nn.functional as F
+import types
 
 
 class NLL(Measurement):
@@ -17,21 +15,30 @@ class NLL(Measurement):
         experiment_config: DictConfig,
     ):
         super().__init__(datamodule_names, model, experiment_config)
-        self.model.test_step = self.test_step
 
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self.model(x)
-        loss = F.cross_entropy(y_hat, y)
-        return loss
+    def make_new_test_step(self, datamodule_name):
+        def test_step(self, batch, batch_idx):
+            x, y = batch
+            y_hat = self.model(x)
+            loss = F.cross_entropy(y_hat, y)
+            self.log(f"{datamodule_name}_nll", loss, on_epoch=True)
+            return loss
+
+        return test_step
 
     def measure(self):
-        results = dict()
+        results_dict = dict()
 
         for datamodule_name, datamodule in self.datamodules.items():
-            results[f"{datamodule_name}_nll"] = self.trainer.test(
+
+            new_test_step = self.make_new_test_step(datamodule_name)
+            # self.model.test_step = new_test_step
+            self.model.test_step = types.MethodType(new_test_step, self.model)
+            results = self.trainer.test(
                 self.model,
                 datamodule=datamodule,
             )
+            for d in results:
+                results_dict.update(d)
 
-        return results
+        return results_dict
