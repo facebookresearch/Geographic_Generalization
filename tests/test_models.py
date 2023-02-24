aef4e5e35b4e38519d016f3809af22d71e6e357d
Megan Richards <meganrichards@devfair0175.h2.fair>
2023-02-23 17:27:49 -0800

    tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,20 +7,25 @@ from models.resnet.resnet import (
     ResNet101dClassifierModule,
 )
 from models.mlp_mixer.mlp_mixer import MLPMixerClassifierModule
-from models.vit.vit import VitClassifierModule
+from models.vit.vit import VitClassifierModule, VitLargeClassifierModule
 from models.beit.beit import BeitClassifierModule
+from models.simclr.simclr import SimCLRClassifierModule
+from models.convnext.convnext import ConvNextlassifierModule
 
 
 @pytest.mark.webtest
 class TestPreTrainedModels:
     batch_size = 8
     models = [
-        MLPMixerClassifierModule(),
-        ResNet18dClassifierModule(),
-        ResNet50dClassifierModule(),
-        ResNet101dClassifierModule(),
-        VitClassifierModule(),
-        BeitClassifierModule(),
+        # MLPMixerClassifierModule(),
+        # ResNet18dClassifierModule(),
+        # ResNet50dClassifierModule(),
+        # ResNet101dClassifierModule(),
+        # VitClassifierModule(),
+        # BeitClassifierModule(),
+        # VitLargeClassifierModule(),
+        # ConvNextlassifierModule(),
+        SimCLRClassifierModule(),
     ]
 
     @pytest.mark.parametrize("model", models)
@@ -33,5 +38,6 @@ class TestPreTrainedModels:
     def test_embedding_shape(self, model):
         x = torch.rand(self.batch_size, 3, 224, 224)
         embedding = model.forward_features(x)
+        # print(embedding)
         assert len(embedding.shape) == 2
         assert embedding.shape[0] == self.batch_size
