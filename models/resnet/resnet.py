815e915bbc3d5fa60654510eee13cd8ab82bc232
Megan Richards <meganrichards@meta.com>
2023-03-01 14:48:24 -0500

    Update resnet.py

--- models/resnet/resnet.py
+++ models/resnet/resnet.py
@@ -1,6 +1,5 @@
 import torch.nn.functional as F
 from models.classifier_model import ClassifierModule
-import torchvision
 
 
 class ResNet50dClassifierModule(ClassifierModule):
@@ -38,18 +37,3 @@ class ResNet18dClassifierModule(ClassifierModule):
         checkpoint_url: str = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/resnet18d_ra2-48a79e06.pth",
     ):
         super().__init__(timm_name=timm_name, checkpoint_url=checkpoint_url)
-
-
-class ResNet152dClassifierModule(ClassifierModule):
-    def __init__(
-        self,
-        timm_name: str = "",
-        learning_rate: float = 1e-4,
-        optimizer: str = "adam",
-        checkpoint_url: str = "",
-    ):
-        super().__init__(timm_name=timm_name, checkpoint_url=checkpoint_url)
-
-    def load_backbone(self):
-        model = torchvision.models.resnet152(pretrained=True)
-        return model
