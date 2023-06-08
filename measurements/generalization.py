89ee6ca1f9376f692c7376ab6f624ffc2f5b5ed6
Megan Richards <meganrichards@devfair0175.h2.fair>
2023-06-08 10:01:51 -0700

    removed irrelevant parts

--- measurements/generalization.py
+++ measurements/generalization.py
@@ -1,3 +1,10 @@
+from omegaconf import DictConfig
+import torchmetrics
+import torch.nn.functional as F
+from models.classifier_model import ClassifierModule
+from measurements.measurement_utils import Measurement
+import types
+
 from measurements.measurement_utils import Measurement
 from omegaconf import DictConfig
 import types
@@ -309,5 +316,90 @@ class GeodePerformance(Measurement):
             )
 
             return
+
+        return new_test_step
+
+
+class ClassificationAccuracyEvaluation(Measurement):
+    """
+    Commented Example of a Measurement (Accuracy on new datamodule)
+
+    The base class constructor creates the following objects for you to use:
+        1) self.datamodules: a dictionary of datamodules, indexed by the datamodule's name (e.g. {datamdoule_name : datamodule}). This dictionary contains a datamodule for each value passed in the parameter 'datamodule_names', so will *only* include datamodules for
+            those datamodules. To access a datamodule, simply call next(iter(self.datamodules.items()) as seen below to get the datamodule name (key) and datamodule (value), or index self.datamodules dictionary with datamodule name you want (E.g.: self.datamodules['imagenet']).
+
+        2) self.model: the instantiated model object to use in the measurement.
+
+    The base class also has a function 'save_extra_results_to_csv' which can be used to save model predictions, or other details.
+    """
+
+    def __init__(
+        self,
+        datamodule_names: list[str],
+        model: ClassifierModule,
+        experiment_config: DictConfig,
+    ):
+        super().__init__(
+            datamodule_names=datamodule_names,
+            model=model,
+            experiment_config=experiment_config,
+        )
+
+    def measure(
+        self,
+    ) -> dict[str:float]:
+        # 1) Make a dict to store measurements
+        results_dict = {}
+
+        for data_module_name, datamodule in self.datamodules.items():
+            print(data_module_name)
+
+            # 2) Access datamodules needed - self.datamodules is a dict mapping from datamodule names (str) to datamodules. E.g. {'imagenet': ImageNetDatamodule object}
+            datamodule_name, datamodule = next(iter(self.datamodules.items()))
+
+            # 3) Define the measurement by overwriting the test step for the model
+            new_test_step = self.make_new_test_step(
+                datamodule_name=datamodule_name, mask=datamodule.mask
+            )
+
+            self.model.test_step = types.MethodType(new_test_step, self.model)
+            print("made new test step")
+
+            # Call validate / test function
+            results = self.trainer.test(model=self.model, datamodule=datamodule)
+
+            # 5) Format results into a dictionary and return
+            for d in results:
+                results_dict.update(d)
+
+            # Optional: save predictions
+            if self.save_detailed_results:
+                self.save_extra_results_to_csv(
+                    extra_results=self.model.predictions,
+                    name=f"{datamodule_name}_predictions",
+                )
+
+        return results_dict  # to be added to CSV
+
+    def make_new_test_step(self, datamodule_name, mask):
+        # The whole purpose of this wrapper function is to allow us to pass in the 'datamodule_name' for logging.
+        # We cannot pass it into new_test_step directly without changing the signature required by trainer.
+
+        def new_test_step(self, batch, batch_idx):
+            x, y = batch
+            if mask is not None:
+                y_hat = self(x)[:, mask]
+            else:
+                y_hat = self(x)
+            # If you make a torchmetrics metric outside of the model construction, it doesn't get automatically moved to a device
+
+            result = self.test_accuracy(F.softmax(y_hat, dim=-1), y)
+            self.log(f"{datamodule_name}_test_accuracy", result, on_epoch=True)
+
+            self.save_predictions(
+                {"prediction": y_hat.cpu().tolist(), "label": y.cpu().tolist()}
+            )
+
+            return result
 
         return new_test_step
--- tests/test_measurements.py
+++ tests/test_measurements.py
@@ -4,9 +4,6 @@ from hydra.utils import instantiate
 import copy
 from measurements.measurement_utils import Measurement
 import pytorch_lightning as pl
-from measurements.properties.equivariance.equivariance import Equivariance
-from measurements.properties.sparsity.sparsity import Sparsity
-from measurements.properties.calibration.calibration import ECE, NLL
 import pytest
 import torch
 import hydra
@@ -45,93 +42,6 @@ class TestMeasurementSetUp:
         hydra.core.global_hydra.GlobalHydra.instance().clear()
 
 
-@pytest.mark.webtest
-class TestEquivariance:
-    @pytest.fixture(scope="module")
-    def equivariance_measure(self):
-        initialize(version_base=None, config_path="../config/")
-        experiment_config = compose(config_name="test.yaml")
-        model = instantiate(experiment_config.model)
-        equivariance = Equivariance(["dummy"], model, experiment_config)
-        return equivariance
-
-    def test_test_step(self, equivariance_measure: Equivariance):
-        batch_size = 32
-        batch = (
-            torch.rand(batch_size, 3, 224, 224),
-            torch.randint(10, (batch_size, 1)),
-        )
-        equivariance_measure.reset_stored_z()
-        equivariance_measure.test_step(batch, 0)
-        assert equivariance_measure.z.shape == (
-            batch_size,
-            512,
-        )
-        # embedding dim x number of transformation parameters
-        assert equivariance_measure.z_t.shape == (batch_size, 512, 10)
-
-    def test_embeddings_are_stored(self, equivariance_measure: Equivariance):
-        equivariance_measure.reset_stored_z()
-        equivariance_measure.measure()
-        num_batches = equivariance_measure.experiment_config.trainer.limit_test_batches
-        assert equivariance_measure.z.shape == (num_batches * 32, 512)
-        assert equivariance_measure.z_t.shape == (num_batches * 32, 512, 10)
-
-    def test_results(self, equivariance_measure: Equivariance):
-        equivariance_measure.reset_stored_z()
-        results = equivariance_measure.measure()
-
-        assert "dummy_equivariance_rotate" in results
-        assert results["dummy_equivariance_rotate"] > 0.0
-        assert "dummy_invariance_rotate" in results
-        assert results["dummy_invariance_rotate"] > 0.0
-
-    def test_shuffle_z_t(self, equivariance_measure: Equivariance):
-        z_t = torch.rand(8, 512, 10)
-        z_t_shuffled = equivariance_measure.shuffle_z_t(z_t)
-        assert z_t.shape == z_t_shuffled.shape
-        assert not torch.allclose(z_t, z_t_shuffled)
-
-
-@pytest.mark.webtest
-class TestSparsity:
-    @pytest.fixture(scope="module")
-    def sparsity_measure(self):
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-        initialize(version_base=None, config_path="../config/")
-        experiment_config = compose(config_name="test.yaml")
-        model = instantiate(experiment_config.model)
-        sparsity = Sparsity(["dummy"], model, experiment_config)
-        return sparsity
-
-    def test_test_step(self, sparsity_measure: Sparsity):
-        batch_size = 8
-        batch = (
-            torch.rand(batch_size, 3, 224, 224),
-            torch.randint(10, (batch_size, 1)),
-        )
-        sparsity_measure.reset_stored_z()
-        sparsity_measure.test_step(batch, 0)
-        assert sparsity_measure.z.shape == (
-            batch_size,
-            512,
-        )
-
-    def test_embeddings_are_stored(self, sparsity_measure: Sparsity):
-        sparsity_measure.reset_stored_z()
-        sparsity_measure.measure()
-        num_batches = sparsity_measure.experiment_config.trainer.limit_test_batches
-        assert sparsity_measure.z.shape == (num_batches * 32, 512)
-
-    def test_results(self, sparsity_measure: Sparsity):
-        sparsity_measure.reset_stored_z()
-        results = sparsity_measure.measure()
-
-        assert len(results) > 0
-        assert "dummy_sparsity_1" in results
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-
-
 class TestGeneralization:
     hydra.core.global_hydra.GlobalHydra.instance().clear()
     initialize(version_base=None, config_path="../config/")
@@ -164,49 +74,4 @@ class TestGeneralization:
         for val in list(result.values()):
             assert val is not None
             assert val > 0.0
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-
-
-@pytest.mark.webtest
-class TestNLL:
-    @pytest.fixture(scope="module")
-    def nll_measure(self):
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-        initialize(version_base=None, config_path="../config/")
-        experiment_config = compose(config_name="test.yaml")
-        model = instantiate(experiment_config.model)
-        nll = NLL(["dummy"], model, experiment_config)
-        return nll
-
-    def test_results(self, nll_measure: NLL):
-        results = nll_measure.measure()
-        assert "dummy_calibration_nll" in results
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-
-
-@pytest.mark.webtest
-class TestECE:
-    @pytest.fixture(scope="module")
-    def ece_measure(self):
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-        initialize(version_base=None, config_path="../config/")
-        experiment_config = compose(config_name="test.yaml")
-        model = instantiate(experiment_config.model)
-        ece = ECE(["dummy"], model, experiment_config)
-        return ece
-
-    def test_ece_measure(self, ece_measure: ECE):
-        preds = np.ones((2, 100))
-        preds[0] *= 0.6 / 99
-        preds[0][0] = 0.4
-        preds[1] *= 0.1 / 99
-        preds[1][0] = 0.9
-        targets = np.array([1, 0])
-        n_bins = 2
-        ece_val = ece_measure.measure_ece(preds, targets, n_bins)
-        assert ece_val == 0.25
-
-    def test_results(self, ece_measure: ECE):
-        results = ece_measure.measure()
-        assert "dummy_calibration_ece" in results
         hydra.core.global_hydra.GlobalHydra.instance().clear()
